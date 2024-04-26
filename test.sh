106e937736909af53725b7d8e945b1dcb065d6b0
romangd70 <168233178+romangd70@users.noreply.github.com>
2024-04-27 00:19:26 +0300

    Add files via upload

--- test.sh
+++ test.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
+
 input_dir=$1
 output_dir=$2
 
 mkdir -p "$output_dir"
 
 find "$input_dir" -type f | while read -r file; do
-    base=$(basename "$file")
-    if [ ! -f "$output_dir/$base" ]; then
+    base_name=$(basename "$file")
+    if [ ! -f "$output_dir/$base_name" ]; then
         
-        cp "$file" "$output_dir/$base"
+        cp "$file" "$output_dir/$base_name"
     else
                
-	ct=1
-        new="${base%.*}_$ct.${base_name##*.}"
-        while [ -f "$output_dir/$new" ]; do
+	counter=1
+        new_name="${base_name%.*}_$counter.${base_name##*.}"
+        while [ -f "$output_dir/$new_name" ]; do
             counter=$((counter + 1))
-            new="${base%.*}_$ct.${base##*.}"
+            new_name="${base_name%.*}_$counter.${base_name##*.}"
         done
-        cp "$file" "$output_dir/$new"
+        cp "$file" "$output_dir/$new_name"
     fi
 done
+
